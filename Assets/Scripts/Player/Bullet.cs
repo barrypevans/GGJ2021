eef1f4728dc9f2cae35f2e6bf3a74016c2c1a42f
barry <barry.p.evans@gmail.com>
2021-01-30 17:23:10 -0800

    [*] art integration

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -27,4 +27,10 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject != GameManager.Get().GetPlayer())
+            Destroy(gameObject);
+    }
 }
