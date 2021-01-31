25c0a120aa25f31367e642ebbe6fd14c0fabe19e
barry <barry.p.evans@gmail.com>
2021-01-31 15:19:44 -0800

    [+] hit pause

--- Assets/Scripts/FX/FXManager.cs
+++ Assets/Scripts/FX/FXManager.cs
@@ -10,6 +10,9 @@ public class FXManager : SystemSingleton<FXManager>
 
     private Queue<AudioSource> m_sfxQueue;
 
+    private double m_hitPauseStartTime = 0;
+    private bool m_isHitPausing = false;
+
     // Start is called before the first frame update
     void Start() //Only works with Awake()???
     {
@@ -25,18 +28,22 @@ public class FXManager : SystemSingleton<FXManager>
         m_backTrackManager = new GameObject("MusicSource");
         m_backTrackManager.AddComponent<AudioSource>();
 
-        
+
         //Instantiate(m_fxManager);
         //Debug.Log("started FXManager");
 
         //Sets the music
-        SetMusic("music/title_screen_theme_1");    
+        SetMusic("music/title_screen_theme_1");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (m_isHitPausing &&
+            Time.realtimeSinceStartupAsDouble - m_hitPauseStartTime > .03)
+        {
+            m_isHitPausing = false;
+            Time.timeScale = 1;
+        }
     }
 
     //Sets music to given filename
@@ -71,5 +78,12 @@ public class FXManager : SystemSingleton<FXManager>
         sfxChannel.volume = volume;
         sfxChannel.PlayOneShot(sfx);
         m_sfxQueue.Enqueue(sfxChannel);
+    }
+
+    public void DoHitPause()
+    {
+        m_hitPauseStartTime = Time.realtimeSinceStartupAsDouble;
+        m_isHitPausing = true;
+        Time.timeScale = 0;
     }
 }
