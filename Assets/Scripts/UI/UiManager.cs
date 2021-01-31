d6e6d58534eb97d312332bcfbf3dcde39f7af226
kdeppmeier <kdeppmeier@yahoo.com>
2021-01-31 11:26:15 -0800

    Implemented Pause Popup Panel

--- Assets/Scripts/UI/UiManager.cs
+++ Assets/Scripts/UI/UiManager.cs
@@ -10,6 +10,7 @@ public class UiManager : SystemSingleton<UiManager>
     private GameObject m_canvas;
     private GameObject m_mainMenuPanel;
     private GameObject m_gameplayPanel;
+    private GameObject m_pausePanel;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +19,20 @@ public class UiManager : SystemSingleton<UiManager>
         var canvas = m_ui.transform.GetChild(0);
         m_mainMenuPanel = canvas.GetChild(0).gameObject;
         m_gameplayPanel = canvas.GetChild(1).gameObject;
+        m_pausePanel = canvas.GetChild(2).gameObject;
         m_gameplayPanel.SetActive(false);
+        m_pausePanel.SetActive(false);
     }
 
     public void ShowGameplayPanel()
     {
         m_mainMenuPanel.SetActive(false);  //Hides the main menu
         m_gameplayPanel.SetActive(true);  //Unhides the gameplay UI
+    }
+
+    public void ShowPausePanel(bool isPaused)
+    {
+        m_pausePanel.SetActive(isPaused);
     }
 
     public void UpdatePlayerHealth(int healthLevel)
