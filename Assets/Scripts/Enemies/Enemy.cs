865738dbae7210f3b4ea869fde3bbbf28518be26
zuran <gsugrader@gmail.com>
2021-01-31 16:48:12 -0800

    Enemy Power and hit points

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -13,6 +13,14 @@ public class Enemy : Animateable
         Leaping
     }
 
+    public enum Power { 
+        Easy,
+        Hard
+    }
+
+    public int HitPoints = 1;
+    public Power EnemyPower = Power.Easy;
+
     private Vector2 _startPosition;
     // Start is called before the first frame update
     void Start()
